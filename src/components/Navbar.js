7ffb4e3155d0515c48b8e9295cb6cf61981bccfe
bishwajit70 <mailtobishwa15@gmail.com>
2022-05-19 05:02:42 +0600

    signout added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,23 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import auth from '../firebase.init';
 
 const Navbar = () => {
     const [user, loading, error] = useAuthState(auth);
+    const navigate = useNavigate()
+    const handleSignOut = () => {
+        signOut(auth)
+        navigate('/login')
+    }
     return (
         <div className='py-5 bg-purple-700 text-white'>
             <Link className='p-2' to='/'>Home</Link>
 
             {user ? <Link className='p-2' to='/addtask'>Add Task</Link> : ""}
-            <Link className='p-2' to='/login'>Login</Link>
+
+            {user ? <Link onClick={() => handleSignOut()} className='p-2' to='/login'>Sign Out</Link> : <Link className='p-2' to='/login'>Login</Link>}
         </div>
     );
 };
